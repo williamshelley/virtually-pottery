9108327c6c73324c7d963d66e3ebb9e66e7a1916
William Shelley <wjshelley5018@gmail.com>
2020-08-17 00:04:54 -0400

    Added onClick handler

--- entry.js
+++ entry.js
@@ -14,33 +14,125 @@ const {
   h2Node,
   h3Node
 } = require("./src/html_util.js");
+const {
+  createScene,
+  createCamera,
+  createRenderer,
+  animate,
+} = require("./src/util/threejs_util.js");
 
+// var THREE = require('three'); // require peer dependency
+// var initializeDomEvents = require('threex.domevents');
+import * as THREE from "three";
+// const initializeDomEvents = require("threex.domevents");
+// import * as THREEx from "threex.domevents";
 
+// const scene = new THREE.Scene();
+// import "./assets/stylesheets/default.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // var THREEx = {};
+  // initializeDomEvents(THREE, THREEx);
+  // console.log(initializeDomEvents);
+
   const root = document.getElementById("root");
-  root.innerHTML += div({
-    children: [
-      h1({
-        children: ["h1", "Testing", "1", "2", p({
-          children: ["3"]
-        })]
-      }),
-      h2({
-        children: ["h2"]
-      }),
-      h3({
-        children: ["h3"]
-      })
-    ]
+  // root.innerHTML += div({
+  //   children: [
+  //     h1({
+  //       children: ["h1", "Testing", "1", "2", p({
+  //         children: ["3"]
+  //       })]
+  //     }),
+  //     h2({
+  //       children: ["h2"]
+  //     }),
+  //     h3({
+  //       children: ["h3"]
+  //     })
+  //   ]
+  // });
+
+  // let c1 = h3Node({
+  //   innerText: "h3 child of h1"
+  // });
+  // let c2 = h3Node({
+  //   innerText: "h3 child of h1"
+  // });
+  // let c3 = h3Node({
+  //   innerText: "h3 child of h1"
+  // });
+  // let h1Child = divNode({
+  //   innerText: "This is an h1 child!",
+  //   children: [c1, c2, c3]
+  // });
+
+  // h1Child.addEventListener("click", () => console.log("hello"))
+
+  // root.appendChild(h1Child);
+
+  var scene = createScene();
+  var camera = createCamera();
+  var renderer = createRenderer();
+  const loader = new THREE.TextureLoader();
+
+  let shelfLength = 50;
+  let shelfHeight = 32;
+
+  let rightShelfGeo = new THREE.PlaneGeometry(shelfLength, 20, shelfHeight);
+  let leftShelfGeo = new THREE.PlaneGeometry(shelfLength, 20, shelfHeight);
+
+  var shelfMat = new THREE.MeshBasicMaterial({
+    map: loader.load('assets/images/empty_shelf.jpg'),
   });
+
+  let initialShelfTranslationZ = -10;
+  let initialShelfTranslationX = 8
+  let initialShelfRotationY = 1.5
+
+  let rightShelf = new THREE.Mesh(rightShelfGeo, shelfMat);
+  rightShelf.translateZ(initialShelfTranslationZ);
+  rightShelf.translateX(initialShelfTranslationX);
+  rightShelf.rotation.y = -initialShelfRotationY;
   
-  let c1 = h3Node({ innerText: "h3 child of h1" });
-  let c2 = h3Node({ innerText: "h3 child of h1" });
-  let c3 = h3Node({ innerText: "h3 child of h1" });
-  let h1Child = divNode({ innerText: "This is an h1 child!", children: [c1,c2,c3]});
-  
-  h1Child.addEventListener("click", () => console.log("hello"))
-  
-  root.appendChild(h1Child);
+  let leftShelf = new THREE.Mesh(leftShelfGeo, shelfMat);
+  leftShelf.translateZ(initialShelfTranslationZ);
+  leftShelf.translateX(-initialShelfTranslationX);
+  leftShelf.rotation.y = initialShelfRotationY;
+
+
+  var cube = new THREE.Mesh(
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({
+      color: 0xffffff
+    })
+  );
+
+  var raycaster = new THREE.Raycaster();
+  var eventListenerInfo = {
+    raycaster,
+    scene,
+    camera
+  };
+
+  cube.position.z = -10;
+  cube.value = "YAY!";
+  scene.add(cube);
+  scene.add(rightShelf);
+  scene.add(leftShelf);
+
+  root.appendChild(renderer.domElement);
+
+  cube.onClick(obj => console.log(obj.value), eventListenerInfo);
+  rightShelf.onClick(obj => console.log("right"), eventListenerInfo);
+  leftShelf.onClick(obj => console.log("left"), eventListenerInfo);
+
+  var int = raycaster.intersectObjects(scene.children);
+  console.log(int)
+
+  function animateObjects() {
+    cube.rotation.y += 0.01;
+  }
+
+  window.scene = scene;
+  animate(renderer, scene, camera, animateObjects);
 })
---
+++ src/util/threejs_util.js
@@ -0,0 +1,43 @@
+import * as THREE from 'three';
+
+export function createScene() {
+  return new THREE.Scene();
+}
+
+export function createCamera() {
+  // FOV, AR, Near, Far
+  let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 5;
+  return camera;
+}
+
+export function createRenderer() {
+  var renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#webgl-canvas"),
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  return renderer;
+}
+
+export function animate(renderer, scene, camera, action) {
+  let _animate = animate.bind(this, renderer, scene, camera, action);
+  requestAnimationFrame( _animate );
+  action({ scene, camera });
+  renderer.render(scene, camera);
+}
+
+THREE.Mesh.prototype.onClick = function(action, { raycaster, scene, camera }) {
+  let mouse = new THREE.Vector2();
+  window.addEventListener("click", event => {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+    let intersects = raycaster.intersectObjects(scene.children);
+    for ( let i = 0; i < intersects.length; i++ ) {
+      if (intersects[i].object.uuid === this.uuid) {
+        action(this);
+      }
+    }
+  })
+}
